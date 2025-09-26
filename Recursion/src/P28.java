52a310cb30466779487d6e39d64085853968ac1f
RoyNisimov <roynisimov@gmail.com>
2025-09-26 14:36:12 +0300

    Fix

--- Recursion/src/P28.java
+++ Recursion/src/P28.java
@@ -1,14 +1,14 @@
 public class P28 {
 
     public static void main(String[] args) {
-        ex24('a', 'z');
+        ex24('b', 'o');
     }
 
     public static void ex24(char beginChar, char endChar){
         String lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
         int bI = lowerCaseAlphabet.indexOf(beginChar);
         int eI = lowerCaseAlphabet.indexOf(endChar);
-        if (bI>eI) return;
+        if (bI+1>=eI) return;
         if (beginChar == 'y') return;
         System.out.print(lowerCaseAlphabet.charAt(bI+1));
         ex24(lowerCaseAlphabet.charAt(bI+1), endChar);
